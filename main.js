4279d091db102038bdd649453c5bfcd4d5f145ac
Sridatt Bhamidipati <sbhamidi@ucdavis.edu>
2015-10-11 22:41:51 -0700

    asdf

--- main.js
+++ main.js
@@ -13,3 +13,26 @@ function moveVehicle() {
 
 var animateObj = setInterval(moveVehicle, 25); //sets intervals
 
+function moveCloud1() {
+	var xpos = $("#cloud1").offset().left;
+	
+	//move horizontally
+	$("#cloud1").css({left: xpos + 2})
+	if ( xpos + 5 > screen.width + 200) {
+		$("#cloud1").css({left: -200})
+	}
+}
+
+var animateCloud1 = setInterval(moveCloud1, 25); //sets intervals
+
+function moveCloud2() {
+	var xpos = $("#cloud2").offset().left;
+	
+	//move horizontally
+	$("#cloud2").css({left: xpos + 1})
+	if ( xpos + 5 > screen.width + 200) {
+		$("#cloud2").css({left: -200})
+	}
+}
+
+var animateCloud2 = setInterval(moveCloud2, 25); //sets intervals
