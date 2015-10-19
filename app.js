bff0940e62c62936dc4c25c12bf19a7cb72a6712
Sridatt Bhamidipati <sbhamidi@ucdavis.edu>
2015-10-18 21:09:05 -0700

    firebase integration

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-var fs = require("fs");
-var host = "localhost";
-var port = 3000;
-var express = require("express");
+var fs = require("fs"),
+	express = require("express"),
+	Firebase = require("firebase");
+
+//SETUP
 
 var app = express();
 app.use(express.static(__dirname + "/views")); //use static files in ROOT/public folder
 
+var myFirebaseRef = new Firebase("https://hackdavis.firebaseio.com/");
+
+app.listen("3000", "localhost");
+
+//ROUTES
+
 app.get("/", function(req, res){ //root dir
-    //res.send("Hello!!");
     res.render("index.html")
 });
 
-app.listen(port, host);
+app.post('/saveEmail/:email', function(req, res){
+	myFirebaseRef.push({
+		email: req.params.email
+	})
+	res.send("success")
+})
--- views/js/main.js
+++ views/js/main.js
@@ -82,10 +82,32 @@ var animateCloud2 = setInterval(moveCloud2, 25); //sets intervals
 // });
 
 function submitClicked() {
+	var email = $("#inp").val();
 	$("#inp").val('');
-	$("#msg").text('Thanks for submitting!');
+
+	if (validateEmail(email)){
+		console.log("email is valid");
+		$.ajax({
+			method: 'POST',
+			dataType: "json",
+			url: "/saveEmail/" + email, 
+			data: {},
+			success: function(result){
+	        	console.log(result);
+	        }
+		});
+		$("#msg").text('Thanks for submitting!');
+	}
+	else {
+		$("#msg").text('This email isn\'t valid.');
+		//console.log("email is not valid");
+	}
 }
 
+function validateEmail(email) {
+    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    return re.test(email);
+}
 
 
 
